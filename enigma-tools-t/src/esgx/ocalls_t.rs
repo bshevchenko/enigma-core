4ce272be7e52535dddfc546569c6c5db244b4ab7
Elichai Turkel <elichai@enigma.co>
2019-05-28 12:38:47 +0300

    DEV: Removed the manual ocalls from enigma-tools-t

--- enigma-tools-t/src/esgx/ocalls_t.rs
+++ enigma-tools-t/src/esgx/ocalls_t.rs
@@ -5,17 +5,12 @@ use std::{path::PathBuf, str};
 
 const PATH_MAX: usize = 4096; // linux/limits.h - this depends on the FS.
 
-extern "C" {
-    fn ocall_get_home(output: *mut u8, result_len: &mut usize) -> sgx_status_t;
-    fn ocall_save_to_memory(ptr: *mut u64, data_ptr: *const u8, data_len: usize) -> sgx_status_t;
-}
-
-pub fn get_home_path() -> Result<PathBuf, EnclaveError> {
+pub fn get_home_path(ocall: unsafe extern "C" fn(*mut u8, *mut u32) -> sgx_status_t) -> Result<PathBuf, EnclaveError> {
     // Get Home path via Ocall
     let mut home_slice: [u8; PATH_MAX] = [0; PATH_MAX];
-    let mut result_len: usize = 0;
-    unsafe { ocall_get_home(home_slice.as_mut_ptr(), &mut result_len); }
-    let home_str = str::from_utf8(&home_slice[..result_len])?;
+    let mut result_len = 0;
+    unsafe { ocall(home_slice.as_mut_ptr(), &mut result_len); }
+    let home_str = str::from_utf8(&home_slice[..result_len as usize])?;
     debug_println!("Back from Ocall: {}", &home_str);
 
     Ok(PathBuf::from(home_str))
@@ -23,9 +18,9 @@ pub fn get_home_path() -> Result<PathBuf, EnclaveError> {
 
 
 // TODO: Replace u64 with *const u8, and pass it via the ocall using *const *const u8
-pub fn save_to_untrusted_memory(data: &[u8]) -> Result<u64, EnclaveError> {
+pub fn save_to_untrusted_memory(data: &[u8], ocall: unsafe extern "C" fn(*mut u64, *const u8, usize) -> sgx_status_t) -> Result<u64, EnclaveError> {
     let mut ptr = 0u64;
-    match unsafe { ocall_save_to_memory(&mut ptr as *mut u64, data.as_c_ptr(), data.len()) } {
+    match unsafe { ocall(&mut ptr as *mut u64, data.as_c_ptr(), data.len()) } {
         sgx_status_t::SGX_SUCCESS => Ok(ptr),
         e => Err(e.into()),
     }
